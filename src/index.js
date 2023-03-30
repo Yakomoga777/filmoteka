d52b19e56f5cb1581f54eeb72d453ea25075a4de
Roman Petrenko <rpetrenko0105@gmail.com>
2023-03-30 14:29:27 +0300

    Підключив шрифти, добавив змінні до стилів variables

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/fetch/fetch';
+import './js/local-storage/add-to-watched-queue/add-to-watched-queue';
+import './js/local-storage/show-watched-queue/show-watched-queqe';
+import './js/details-modal/details-modal';
+import './js/loader/loader';
+import './js/firebase/firebase';
+import './js/team-modal/team-modal';
+import './js/trailer/trailer';
