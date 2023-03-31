21368ee4051f36e4934184c88102b31f041617d8
AntonZinchenko32 <zinchenko309@gmail.com>
2023-03-31 12:41:34 +0300

    1
    Подтягивает обновлённый main, убирает кнопку имитатора с main.html, убирает is-hidden с модалки и функции открытия из скрипта

--- src/js/details-modal/details-modal.js
+++ src/js/details-modal/details-modal.js
@@ -1,11 +1,11 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
+      // openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
+    // refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
