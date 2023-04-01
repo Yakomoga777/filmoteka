fdbe22ca77f6257262fa8391ce7757488bd89ed3
MarinaChorna <mrnchrn06@gmail.com>
2023-04-02 00:43:00 +0300

    Оновлення

--- src/js/details-modal/details-modal.js
+++ src/js/details-modal/details-modal.js
@@ -3,8 +3,7 @@ import {moviesApiService} from '../fetch/fetch';
 const cardsFilms = document.querySelectorAll('.list_film_item');
 const modalEl = document.querySelector('div[data-modal]');
 const modalWindow = document.querySelector('.details-modal');
-const buttonClose = document.querySelector('.close-button');
-console.log(buttonClose);
+let buttonClose;
 
 function renderCard(data) {
     modalWindow.innerHTML='';
@@ -62,6 +61,11 @@ function renderCard(data) {
     </div>
   </div>`
   modalWindow.insertAdjacentHTML('beforeend', oneCard);
+  buttonClose = document.querySelector('.close-button');
+  buttonClose.addEventListener('click', function() {
+    modalEl.classList.add('is-hidden');
+    console.log('click');
+});
 }
 
 
@@ -82,11 +86,6 @@ for (const card of cardsFilms) {
         modalEl.addEventListener('click', function clickOnBackdrop(e) {
             if (e.target===e.currentTarget) {
                 modalEl.classList.add('is-hidden');
-            };
-        });
-        buttonClose.addEventListener('click', function() {
-            modalEl.classList.add('is-hidden');
-            console.log('click');
-        })
-  })
+            };})
+  });
 }
