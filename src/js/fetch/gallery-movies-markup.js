6067656618ff48e1c0c7a1460e92766e74bd313f
Olena Savchenko <elena.volobuieva@gmail.com>
2023-04-01 13:25:34 +0300

    импортує екземпляр классу

---
+++ src/js/fetch/gallery-movies-markup.js
@@ -0,0 +1,9 @@
+import { moviesApiService } from './fetch';
+
+// moviesApiService.fetchTrendingMovies().then(data=> console.log(data)).catch(error => console.log(error));
+
+// async function createMoviesGallery() {
+//   const movies = await moviesApiService.fetchTrendingMovies();
+// }
+
+// createMoviesGallery().then(movies => console.log(movies));
