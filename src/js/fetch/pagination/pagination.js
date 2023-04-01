9f96c72600272d92523c5c6c8ce3d4eb9ff94b9c
ViktoriyaSF <kievstar01051985@gmail.com>
2023-04-01 21:11:43 +0300

    пагінація з прикладом

--- src/js/fetch/pagination/pagination.js
+++ src/js/fetch/pagination/pagination.js
@@ -6,41 +6,42 @@ import { moviesApiService } from '../fetch';
 const galleryEl = document.querySelector('.list_film.list');
 const paginationEL = document.querySelector('.tui-pagination');
 
-console.log(moviesApiService.fetchTrendingMovies());
+// -------- для перевірки---------
 
-moviesApiService.fetchTrendingMovies().then(data => {
-  galleryFilms(data);
-});
-function galleryFilms(data) {
-    const pagN
-  console.log(data.page);
-  const markup = data.results
-    .map(
-      ({
-        id,
-        original_title,
-        poster_path,
-        title,
-        genre_ids,
-        release_date,
-      }) => `<li class="list_film_item" data-id="${id}">
-        <img src="${renderImg(
-          poster_path
-        )}" alt="${title}"  width="395" height="574" />
-        <h2>${original_title}</h2>
-        <p>${genre_ids} | ${release_date}
-      </li>`
-    )
-    .join('');
+// moviesApiService.fetchTrendingMovies().then(data => {
+//   galleryFilms(data);
+// });
 
-  galleryEl.insertAdjacentHTML('beforeend', markup);
-  function renderImg(poster_path) {
-    if (poster_path) {
-      return `https://image.tmdb.org/t/p/w500${poster_path}`;
-    }
-    return `https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png?20170513175923`;
-  }
-}
+// function galleryFilms(data) {
+//   const markup = data.results
+//     .map(
+//       ({
+//         id,
+//         original_title,
+//         poster_path,
+//         title,
+//         genre_ids,
+//         release_date,
+//       }) => `<li class="list_film_item" data-id="${id}">
+//         <img src="${renderImg(
+//           poster_path
+//         )}" alt="${title}"  width="395" height="574" />
+//         <h2>${original_title}</h2>
+//         <p>${genre_ids} | ${release_date}
+//       </li>`
+//     )
+//     .join('');
+
+//   galleryEl.insertAdjacentHTML('beforeend', markup);
+//   function renderImg(poster_path) {
+//     if (poster_path) {
+//       return `https://image.tmdb.org/t/p/w500${poster_path}`;
+//     }
+//     return `https://upload.wikimedia.org/wikipedia/commons/c/c2/No_image_poster.png?20170513175923`;
+//   }
+// }
+
+// ----------------------
 
 const options = {
   totalItems: 500,
@@ -70,15 +71,13 @@ const options = {
   },
 };
 
-export const pagination = new Pagination(paginationEL, options);
+const pagination = new Pagination(paginationEL, options);
 
-let pagNumber = pagination.on('afterMove', event => {
-  const currentPage = event.page;
-  console.log(currentPage);
+pagination.on('afterMove', async event => {
+  moviesApiService.page = event.page;
+  galleryEl.innerHTML = '';
+  const movies = await moviesApiService.fetchTrendingMovies();
+  console.log(movies);
+  galleryFilms(movies);
+  console.log(moviesApiService.page);
 });
-// console.log(pagNumber);
-
-// galleryEl.innerHTML = '';
-// онулення сторінки
-// CurrentPage;
-// побудувати нову функцію для визову нової сторінки currentPage;
