12f315721826fab8685d72c8c694299f14a1219c
Olena Savchenko <elena.volobuieva@gmail.com>
2023-04-02 10:34:07 +0300

    eкспорт функцій розмітки фільмів на головній

--- src/js/fetch/gallery-movies-markup.js
+++ src/js/fetch/gallery-movies-markup.js
@@ -1,36 +1,36 @@
 import { create } from 'lodash';
 import { moviesApiService } from './fetch';
 import { genres } from './genres';
-console.log(genres);
+// console.log(genres);
 
-const moviesGalleryRef = document.querySelector('.list_film');
+export const moviesGalleryRef = document.querySelector('.list_film');
 
 renderTrandingMovies();
 
-async function renderTrandingMovies() {
+export async function renderTrandingMovies() {
   const { results } = await moviesApiService.fetchTrendingMovies();
-  console.log(results);
+  // console.log(results);
   renderMoviesMarkup(results);
-  console.log(renderMoviesMarkup(results));
+  // console.log(renderMoviesMarkup(results));
 }
 
 function renderMoviesMarkup(movies) {
-  movies.forEach(movie => {
-    const gendersIs = genres.filter(genre => {
-      movie.genres_ids.include(genre.id);
-    });
-  });
+  // movies.forEach(movie => {
+  //   const gendersIs = genres.filter(genre => {
+  //     movie.genres_ids.include(genre.id);
+  //   });
+  // });
   const markup = movies
-    .map(movie => {
-      let urlImg = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    .map(({ poster_path, id, original_title, release_date }) => {
+      let urlImg = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
-      return `<li id="${movie.id}">
-		<img src="${urlImg}" alt="img of film" width="395"/>
-	  <p class="card__title">${movie.original_title}</p>
-	  <div class="card__info">
-    <p class="card__genres">| ${movie.release_date.slice(0, 4)}</p>
-    </div>
-		</li>`;
+      return `<li class="list_film_item" data-id="${id}">
+		            <img src="${urlImg}" alt="img of film" width="395"/>
+	              <p class="card__title">${original_title}</p>
+	              <div class="card__info">
+                  <p class="card__genres">| ${release_date.slice(0, 4)}</p>
+                </div>
+		        </li>`;
     })
     .join('');
 
