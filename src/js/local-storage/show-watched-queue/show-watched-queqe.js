2985ba0772000d12d113f183a113cd8fcfc8fa6e
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-04-02 13:58:03 +0300

    очищаю список

--- src/js/local-storage/show-watched-queue/show-watched-queqe.js
+++ src/js/local-storage/show-watched-queue/show-watched-queqe.js
@@ -3,6 +3,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const STORAGE_KEY = 'picked-movies-array';
 
+moviesGalery.innerHTML = '';
+
 // console.log(moviesApiService.fetchTrendingMovies());
 
 // const showBtn = document.getElementById('showOrder')
@@ -10,6 +12,7 @@ const STORAGE_KEY = 'picked-movies-array';
 // const removeBtn = document.getElementById('removeOrders')
 const moviesGalery = document.getElementById('listMovies')
 const watchedBtn = document.querySelector('[data-action = "watched"]');
+// const watchedBtn = document.querySelector('.library-btn');
 const queuedBtn = document.querySelector('[data-action = "queue"]');
  console.log(watchedBtn);
  console.log(queuedBtn);
@@ -17,7 +20,7 @@ console.log(moviesGalery);
  
 const onWatchedClick = () => {
   moviesGalery.innerHTML = '';
-  
+  onShowStorage();
 }
 
 watchedBtn.addEventListener('click',onWatchedClick)
@@ -68,8 +71,9 @@ function onShowStorage() {
     
   if (arrayOfMovies) {
     try {
-        const parsedValues = JSON.parse(arrayOfMovies);
-        console.log(parsedValues);
+      const parsedValues = JSON.parse(arrayOfMovies);
+      return parsedValues
+        // console.log(parsedValues);
     } catch (error) {
       console.error('Get state error: ', error.message);
     }
@@ -92,4 +96,30 @@ removeBtn.addEventListener('click',clearLocalStorage)
 
 function clearLocalStorage() {
     localStorage.removeItem(STORAGE_KEY);
+}
+
+
+
+
+function renderMoviesMarkup(movies) {
+  // movies.forEach(movie => {
+  //   const gendersIs = genres.filter(genre => {
+  //     movie.genres_ids.include(genre.id);
+  //   });
+  // });
+  const markup = movies
+    .map(({ poster_path, id, original_title, release_date }) => {
+      let urlImg = `https://image.tmdb.org/t/p/w500${poster_path}`;
+
+      return `<li class="list_film_item" data-id="${id}">
+		            <img src="${urlImg}" alt="img of film" width="395"/>
+	              <p class="card__title">${original_title}</p>
+	              <div class="card__info">
+                  <p class="card__genres">| ${release_date.slice(0, 4)}</p>
+                </div>
+		        </li>`;
+    })
+    .join('');
+
+  moviesGalery.insertAdjacentHTML('beforeend', markup);
 }
