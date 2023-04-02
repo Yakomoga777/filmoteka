3f02898682ce6fc76e752e091001329598f59e01
Olena Savchenko <elena.volobuieva@gmail.com>
2023-04-02 10:20:27 +0300

    додає файл з жанрами фільмів genres

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './js/fetch/fetch';
 import './js/fetch/gallery-movies-markup';
+import './js/fetch/genres';
 import './js/local-storage/add-to-watched-queue/add-to-watched-queue';
 import './js/local-storage/show-watched-queue/show-watched-queqe';
 import './js/details-modal/details-modal';
--- src/js/fetch/gallery-movies-markup.js
+++ src/js/fetch/gallery-movies-markup.js
@@ -1,5 +1,7 @@
 import { create } from 'lodash';
 import { moviesApiService } from './fetch';
+import { genres } from './genres';
+console.log(genres);
 
 const moviesGalleryRef = document.querySelector('.list_film');
 
@@ -13,22 +15,24 @@ async function renderTrandingMovies() {
 }
 
 function renderMoviesMarkup(movies) {
+  movies.forEach(movie => {
+    const gendersIs = genres.filter(genre => {
+      movie.genres_ids.include(genre.id);
+    });
+  });
   const markup = movies
     .map(movie => {
       let urlImg = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      return `<li>
-		<img src="${urlImg}"
-      		alt="img of film" width="395"/>
-	 <p class="card__title">${movie.original_title}</p>
-	 <div class="card__info">
-      <p class="card__genres">| ${movie.release_date.slice(0, 4)}</p>
-    </div>
 
+      return `<li id="${movie.id}">
+		<img src="${urlImg}" alt="img of film" width="395"/>
+	  <p class="card__title">${movie.original_title}</p>
+	  <div class="card__info">
+    <p class="card__genres">| ${movie.release_date.slice(0, 4)}</p>
+    </div>
 		</li>`;
     })
     .join('');
-
-  console.log(markup);
 
   moviesGalleryRef.insertAdjacentHTML('beforeend', markup);
 }
