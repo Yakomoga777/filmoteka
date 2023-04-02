98fe4fc4358f997fbe7acb26d50251933da979fb
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-03 00:04:55 +0300

    переписав шлях до картинки

--- src/js/details-modal/details-modal.js
+++ src/js/details-modal/details-modal.js
@@ -40,7 +40,7 @@ function renderCard(data) {
   const oneCard = `
     <button class="close-button" type="button" data-modal-close>
       <svg class="modal-icon-close" width="30px" height="30px">
-        <use href="/icons.adfc4680.svg#icon-close-modal"></use>
+        <use href="./icons.svg#icon-close-modal"></use>
       </svg>
     </button>
     <div class="cover-thumb">
