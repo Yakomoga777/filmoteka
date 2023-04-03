5adaa094fa094be4c4ed740ad0b7c3d658ba64aa
KuhaiAnna <kuhai.anna@gmail.com>
2023-04-04 00:37:56 +0300

    Виправляє помилку
    Додаю інший варіант коду

--- src/js/local-storage/add-to-watched-queue/add-to-watched-queue.js
+++ src/js/local-storage/add-to-watched-queue/add-to-watched-queue.js
@@ -112,7 +112,7 @@ function addToLocalStorage(film, id, key) {
 //           storedItems.push(movie);
 
 //           try {
-//             localStorage.setItem(STORAGE_KEY_W, JSON.stringify(storedItems));
+//             localStorage.setItem(STORAGE_KEY_Q, JSON.stringify(storedItems));
 //           } catch (error) {
 //             console.error('Error adding film to local storage:', error);
 //           }
@@ -128,4 +128,79 @@ function addToLocalStorage(film, id, key) {
 //       }
 //     });
 //   }, 400);
+// }
+
+// ------------ без setTimeout, не працює
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.addEventListener('click', onCardClick);
+// });
+
+// async function onCardClick(e) {
+//   const movieLi = e.target.closest('li');
+//   if (!movieLi) {
+//     return;
+//   }
+
+//   const filmId = Number(movieLi.dataset.id);
+//   const { results } = await moviesApiService.fetchTrendingMovies(); //масив усіх фільмів на цій сторінці
+//   let movie;
+
+//   results.some(movieObject => {
+//     if (filmId === movieObject.id) {
+//       movie = movieObject;
+//       return true;
+//     }
+//   });
+
+//   const addWachedBtn = document.querySelector('.modal-buttons__watched');
+//   const addQueueBtn = document.querySelector('.modal-buttons__queue');
+
+//   addWachedBtn.addEventListener('click', () => {
+//     try {
+//       const storedItems = JSON.parse(localStorage.getItem(STORAGE_KEY_W)) || [];
+
+//       if (!storedItems.some(storedItem => storedItem.id === filmId)) {
+//         storedItems.push(movie);
+
+//         try {
+//           localStorage.setItem(STORAGE_KEY_W, JSON.stringify(storedItems));
+//         } catch (error) {
+//           console.error('Error adding film to local storage:', error);
+//         }
+
+//         // ---------- тимчасово
+//         console.log('Film added to local storage!');
+//       } else {
+//         // ---------- тимчасово
+//         console.log('Filmm already exists in local storage!');
+//       }
+//     } catch (error) {
+//       console.error('Error retrieving films from local storage:', error);
+//     }
+//   });
+
+//   addQueueBtn.addEventListener('click', () => {
+//     try {
+//       const storedItems = JSON.parse(localStorage.getItem(STORAGE_KEY_Q)) || [];
+
+//       if (!storedItems.some(storedItem => storedItem.id === filmId)) {
+//         storedItems.push(movie);
+
+//         try {
+//           localStorage.setItem(STORAGE_KEY_Q, JSON.stringify(storedItems));
+//         } catch (error) {
+//           console.error('Error adding film to local storage:', error);
+//         }
+
+//         // ---------- тимчасово
+//         console.log('Film added to local storage!');
+//       } else {
+//         // ---------- тимчасово
+//         console.log('Filmm already exists in local storage!');
+//       }
+//     } catch (error) {
+//       console.error('Error retrieving films from local storage:', error);
+//     }
+//   });
 // }
