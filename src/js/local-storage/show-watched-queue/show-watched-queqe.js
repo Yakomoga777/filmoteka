2d7806c96d2fb32ac1255689d5d79604e3945899
AntonZinchenko32 <zinchenko309@gmail.com>
2023-04-03 20:40:10 +0300

    Update show-watched-queqe.js

---
+++ src/js/local-storage/show-watched-queue/show-watched-queqe.js
@@ -0,0 +1,77 @@
+import { moviesApiService } from '../../fetch/fetch';
+import { genres } from '../../fetch/genres';
+
+const listFilms = document.querySelector('.list_film');
+
+watchedBtn = document.querySelector('button[data-action="watched"]');
+qeueBtn = document.querySelector('button[data-action="queue"]');
+
+watchedBtn.addEventListener('click', handleClickWatched);
+qeueBtn.addEventListener('click', handleClickQueue);
+
+let moviesArray;
+
+function handleClickWatched() {
+    listFilms.innerHTML = '';
+    moviesArray = JSON.parse(localStorage.getItem('watched-movies-array'));
+    fetchingMovies(moviesArray);
+}
+
+function handleClickQueue() {
+    listFilms.innerHTML = '';
+    moviesArray = JSON.parse(localStorage.getItem('queue-movies-array'));
+    fetchingMovies(moviesArray);
+}
+
+function fetchingMovies(array) {
+    for (const movie of array) {
+    moviesApiService
+        .fetchFilmDetails(movie.id)
+        .then(data => renderMovie(data))
+        .catch(error => console.log(error));
+}
+}
+
+function renderMovie(data) {
+    const {
+        poster_path,
+        id,
+        original_title,
+        release_date,
+        genre_ids,
+        original_language,
+        title,
+    } = data; 
+    
+        let urlImg = '';
+        let movieTitle = '';
+
+        original_language !== 'en'
+          ? (movieTitle = title)
+          : (movieTitle = original_title);
+
+        poster_path
+          ? (urlImg = `https://image.tmdb.org/t/p/w500${poster_path}`)
+          : (urlImg =
+              'https://dummyimage.com/500x400/ff6b08/fff.jpg&text=Opps,+no+image...');
+
+        // const includeGenres = genres.filter(genre =>
+        //   genre_ids.includes(genre.id)
+        // );
+        // const listOfGenres = includeGenres.map(genre => genre.name).join(', ');
+
+        const markup = `<li class="list_film_item" data-id="${id}">
+                <img src="${urlImg}" alt="img of film" width="395"/>
+                <p class="card__title">${movieTitle}</p>
+                <div class="card__info">
+                  <p class="card__genres">NOlistOfGenres(need fix!) | ${
+          release_date.slice(0, 4) ?? ''
+        }</p>
+                </div>
+            </li>`;
+    
+        listFilms.insertAdjacentHTML('beforeend', markup);
+      }
+
+
+  
