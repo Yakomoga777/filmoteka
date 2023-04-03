c590e2ca2b90e0e25559483e0d5b6dab138db563
Olena Savchenko <elena.volobuieva@gmail.com>
2023-04-03 06:51:03 +0300

    дістає назви жанрів по id з gender.js і виводить у шаблонний рядок

--- src/js/fetch/gallery-movies-markup.js
+++ src/js/fetch/gallery-movies-markup.js
@@ -1,7 +1,6 @@
 import { create } from 'lodash';
 import { moviesApiService } from './fetch';
 import { genres } from './genres';
-console.log(genres);
 
 export const moviesGalleryRef = document.querySelector('.list_film');
 
@@ -9,26 +8,36 @@ renderTrandingMovies();
 
 export async function renderTrandingMovies() {
   const results = await moviesApiService.fetchTrendingMovies();
-  // console.log(results);
   renderMoviesMarkup(results);
-  // console.log(renderMoviesMarkup(results));
 }
 
 function renderMoviesMarkup(movies) {
-  // movies.forEach(movie => {
-  //   const gendersIs = genres.filter(genre => {
-  //     movie.genres_ids.include(genre.id);
-  //   });
-  // });
+  console.log('Результати: ', movies.results);
+
+  let listOfGenres = '';
+
+  movies.results.forEach(result => {
+    const includeGenres = genres.filter(genre =>
+      result.genre_ids.includes(genre.id)
+    );
+    listOfGenres = includeGenres.map(genre => genre.name).join(', ');
+    console.log(listOfGenres);
+  });
+
   const markup = movies.results
     .map(({ poster_path, id, original_title, release_date }) => {
+      {
+      }
       let urlImg = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
       return `<li class="list_film_item" data-id="${id}">
 		            <img src="${urlImg}" alt="img of film" width="395"/>
 	              <p class="card__title">${original_title}</p>
 	              <div class="card__info">
-                  <p class="card__genres">| ${release_date.slice(0, 4)}</p>
+                  <p class="card__genres">${listOfGenres} | ${release_date.slice(
+        0,
+        4
+      )}</p>
                 </div>
 		        </li>`;
     })
