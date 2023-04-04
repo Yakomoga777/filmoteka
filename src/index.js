b22d98bd12e8309588cbd482dd4cb4d5653270df
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-04 18:27:26 +0300

    розкоментував предлоадер

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './js/fetch/genres';
 import './js/local-storage/add-to-watched-queue/add-to-watched-queue';
 import './js/local-storage/show-watched-queue/show-watched-queqe';
 import './js/details-modal/details-modal';
-// import './js/loader/loader';
+import './js/loader/loader';
 import './js/firebase/firebase';
 import './js/firebase/firebase-registration-modal';
 import './js/team-modal/team-modal';
