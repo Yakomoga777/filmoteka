66788746992c3a6109f9073e012de38ae8afd042
Roman Petrenko <rpetrenko0105@gmail.com>
2023-04-04 18:58:20 +0300

    Переніс порядок завантаження
    import './js/local-storage/show-watched-queue/show-watched-queqe';

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import './js/fetch/genres';
 // import './js/fetch/searchByName';
 import './js/local-storage/add-to-watched-queue/add-to-watched-queue';
-import './js/local-storage/show-watched-queue/show-watched-queqe';
+
 import './js/details-modal/details-modal';
 // import './js/loader/loader';
 import './js/firebase/firebase';
@@ -20,3 +20,4 @@ import './js/fetch/pagination/btn_up';
 // import './js/fetch/pagination/pagination';
 // import './js/fetch/searchByName';
 import main from './js/main/main';
+import './js/local-storage/show-watched-queue/show-watched-queqe';
